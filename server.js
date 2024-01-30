e1a0cf5c0eeef467e04173206ccf4409fb9e0239
octatesystems <octatesystems@outlook.com>
2024-01-30 23:49:50 +0100

    update repo

--- server.js
+++ server.js
@@ -68,11 +68,12 @@ function(accessToken, refreshToken, profile, done) {
 passport.serializeUser((user, done) => {done(null, user);});
 passport.deserializeUser((user, done) => {done(null, user);});
 
-app.get('/', (req, res) => {
-    res.send(req.session.user) !== undefined? `Logged in as ${req.session.user.displayed}` : "Logged Out" });
+app.get('/', (req, res) => {res.send(req.session.user) !== undefined ? `Logged in as ${req.session.user.displayName}` : "Logged Out" });
 app.get('github/callback', passport.authenticate('github', {
     failureRedirect: '/api-docs', session:false}), (req, res) => {
-        req.session.user = req.user; res.redirect('/'); });
+        req.session.user = req.user; 
+        res.redirect('/'); 
+    });
 
 mongodb.initDb((err) => {
     if (err) {
