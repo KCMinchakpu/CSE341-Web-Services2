059c706c5f0468b9f6fce7217097476a08f31a99
octatesystems <octatesystems@outlook.com>
2024-01-30 18:47:02 +0100

    update repo

--- routes/books.js
+++ routes/books.js
@@ -3,17 +3,18 @@ const router = express();
 
 const booksController = require('../controllers/books');
 const validation = require('../middleware/validate');
+const isAuthenticated = require("../middleware/authenticate");
 
 //Read (Get) All Books Details from the database
 router.get('/', booksController.getAllBooksdetails);
 // Read (GET) a single Book Details from the database
 router.get('/:id', booksController.getSingleBookdetails);
 //Create (POST) a new Book Details
-router.post('/', validation.saveBookdetails, booksController.createBookdetails);
+router.post('/', isAuthenticated, validation.saveBookdetails, booksController.createBookdetails);
 //Update (PUT) a Book Details
-router.put('/:id', validation.saveBookdetails, booksController.updateBookdetails);
+router.put('/:id', isAuthenticated, validation.saveBookdetails, booksController.updateBookdetails);
 //Delete (DELETE) a Book Details
-router.delete('/:id', booksController.deleteBookdetails);
+router.delete('/:id', isAuthenticated, booksController.deleteBookdetails);
 
 
 module.exports = router;
