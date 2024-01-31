531b01e53b2f848d1ef38fc1eaa5a4b06dc99f60
octatesystems <octatesystems@outlook.com>
2024-01-31 02:38:47 +0100

    update repo

--- server.js
+++ server.js
@@ -40,19 +40,7 @@ app
 .use(cors({ methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']}))
 .use(cors({ origin: '*'}))
 .use('/', require('./routes/index'))
-.use(async (req, res, next) => {
-    next({
-        status: 404,
-        message: "Sorry, this route don't exists."
-    });
-})
-.use(async (err, req, res) => {
-    console.error(`Error at: "${req.originalUrl}": ${err.message}`);
-    let message = err.status == 404 ? err.message : 'Sorry, an error occurred in your request.';
-    res.status(err.status || 500).json({
-        error: message
-    });
-});
+
 passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
@@ -69,10 +57,10 @@ passport.serializeUser((user, done) => {done(null, user);});
 passport.deserializeUser((user, done) => {done(null, user);});
 
 app.get('/', (req, res) => { res.send(req.session.user) !== undefined ? `Logged in as ${req.session.user.displayName}` : "Logged Out" });
-app.get('/auth/github/callback', passport.authenticate('github', {
+app.get('/github/callback', passport.authenticate('github', {
     failureRedirect: '/api-docs', session:false}), (req, res) => {
         req.session.user = req.user; 
-        res.redirect('/api-docs'); 
+        res.redirect('/'); 
     });
 
 mongodb.initDb((err) => {
