f766979711174f6f3e562b06bd0ffd509e524206
octatesystems <octatesystems@outlook.com>
2024-01-31 02:58:38 +0100

    update repo

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,7 +1,7 @@
 const isAuthenticated = (req, res, next) => {
     if (req.session.user === undefined) 
     {
-        return res.status(481).json("You do NOT have access to make any Change!");
+        return res.status(481).json("You do NOT have access to make Changes!");
     } next();
 };
 
